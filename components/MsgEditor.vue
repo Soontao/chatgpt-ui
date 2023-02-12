5958326f07d2e32249dae31afaa82cf3e20c63a9
Theo Sun <theo.sun.github@outlook.com>
2023-02-12 13:51:03 +0800

    fix: send button

--- components/MsgEditor.vue
+++ components/MsgEditor.vue
@@ -1,7 +1,7 @@
 <template>
   <v-textarea variant="underlined" v-model="message" clearable label="Message" placeholder="Type your message here" rows="1"
-    :auto-grow="autoGrow" :disabled="disabled" :loading="loading" hide-details append-inner-icon="send"
-    @keyup.enter="send" @click:append="send"></v-textarea>
+    :auto-grow="autoGrow" :disabled="disabled" :loading="loading" hide-details append-inner-icon="send" 
+    @keyup.enter="send" @click:append-inner="send"></v-textarea>
 </template>
 
 <script>
